1d5b84dcd6a796d328b8ee9793c7db0c58db486c
otakukenz <otakukenz@icloud.com>
2025-10-18 13:07:20 +0800

    Initial Commit

---
+++ PhivolcsX/PhivolcsXApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhivolcsXApp.swift
+//  PhivolcsX
+//
+//  Created by Earl Peter de los Reyes on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct PhivolcsXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
